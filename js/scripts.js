2cf3955b2b72c8b8f808d55cbcc0c48f21c0c33e
Dominik Pichler <dominik.pichler@tum.de>
2020-09-25 11:41:29 +0200

    Add on-view Eventlistener for Animations

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
+            location.pathname.replace(/^\//, "") ===
                 this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
@@ -57,11 +57,10 @@
 
 
 
-// ========= CUSTOM ADD'S =========
-
-
 
 /* ======== LOGO Animation Text ======== */
+
+/*
 
 const logo = document.querySelectorAll("#logo path");
 
@@ -70,6 +69,9 @@ for (let i = 0; i < logo.length; i++){
     console.log("1")
     console.log('Letter ' + i +  ' is ' +  logo[i].getTotalLength());
 }
+
+
+ */
 
 /* ======== Sroll Animation About-Section ======== */
 
@@ -84,6 +86,83 @@ window.addEventListener(
     },
     false
 );
+
+
+
+// ========= About Animation =========
+
+
+var svgContainer = document.getElementsByClassName("svg_container")[0];
+
+function isElementInViewport (el) {
+
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+var result = isElementInViewport(svgContainer);
+
+ if (!result && svgContainer.classList.contains("testo")){
+    svgContainer.classList.remove("testo")
+}
+
+
+
+function onVisibilityChange(el, callback) {
+    var old_visible;
+    return function () {
+        var visible = isElementInViewport(el);
+        if (visible !== old_visible) {
+            old_visible = visible;
+            if (typeof callback == 'function') {
+                callback();
+            }
+        }
+    }
+}
+
+
+var handler = onVisibilityChange(svgContainer, function() {
+
+    if(svgContainer.classList.contains("testo")){
+        svgContainer.classList.remove("testo");
+    } else {
+        svgContainer.classList.add("testo");
+    }
+
+});
+
+
+// jQuery
+$(window).on('DOMContentLoaded load resize scroll', handler);
+
+/* // Non-jQuery
+if (window.addEventListener) {
+    addEventListener('DOMContentLoaded', handler, false);
+    addEventListener('load', handler, false);
+    addEventListener('scroll', handler, false);
+    addEventListener('resize', handler, false);
+} else if (window.attachEvent)  {
+    attachEvent('onDOMContentLoaded', handler); // Internet Explorer 9+ :(
+    attachEvent('onload', handler);
+    attachEvent('onscroll', handler);
+    attachEvent('onresize', handler);
+}
+*/
+
+
 
 
 
